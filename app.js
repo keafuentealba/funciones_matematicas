28de43905426ad1e4bd2c0695d2d275c0100ab82
camila fuentealba <camila.fuentealba.rubilar@gmail.com>
2018-05-27 20:19:32 -0400

    js de ejercicio guiado n1

---
+++ app.js
@@ -0,0 +1,12 @@
+let add = (valor1, valor2) => {
+  let suma = valor1 + valor2;
+  return suma;
+}
+
+let multiply = (valor1, valor2) => {
+  let multiplica = valor1 * valor2;
+  return multiplica;
+}
+
+let resultado1 = add(9824, 777);
+let resultado2 = multiply(36325, resultado1);
